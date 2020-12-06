890ff3a3c4f9798da5b6dac5fd923a7283e61e4e
Dhanya110 <73469914+Dhanya110@users.noreply.github.com>
2020-12-06 09:59:12 +0530

    Add files via upload

---
+++ LightGBlock.js
@@ -0,0 +1,23 @@
+class LightGBlock{
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.9,
+          'friction':0.8,
+          'density':1.0,
+          'isStatic':false
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+   fill(204, 255, 153)
+    var pos=this.body.position;
+    rectMode(CENTER)
+      rect(pos.x, pos.y,this.width,this.height);
+      
+      
+     
+    }
+  }
